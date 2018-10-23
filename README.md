168c81e55a4eede0a74f2fd357e93e9857986283
Federico <belerico@pop-os.localdomain>
2018-10-23 17:08:56 +0200

    First reliable commit Docker, Docker compose, redis db

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# PSS_Assignment_1
+# Studenti
+
+    * Luca Brena, matricola: 808216
+    * Federico Belotti, matricola: 808708
+
+## Tecnologie utilizzate
+
+* Containerization: Docker
+* Provisioning: Docker Compose / Kubernetes
+* CI: Gitlab CI
+* CD: Jenkins
+
+### Struttura progetto
+
+Abbiamo creato tre container Docker, contenenti:
+
+* Nginx: reserve-proxy, load-balancer e fornitore di contenuti statici (html, css, js, ...)
+* uWSGI + Flask: uWSGI web server che ospita una semplice applicazione Flask
+* Redis: database key-value
 
--- web/app.py
+++ web/app.py
@@ -1,26 +1,18 @@
-from pyfiglet import Figlet
-import os
-from flask import Flask
+from flask import Flask, request, render_template
+from redis import StrictRedis
 
-app = Flask(__name__)
+application = Flask(__name__)
+db = StrictRedis(host='redis', port=6379, db=0)
 
-font = Figlet(font="starwars")
 
-@app.route("/")
-def main():
-    # get the message from the environmental variable $MESSAGE
-    # or fall back to the string "no message specified"
-    message = os.getenv("MESSAGE", "no message specified")
+@application.route("/greetings", methods=["POST"])
+def greetings():
+        db.incr(request.form.get("username"))
+        name = request.form.get("username")
+        count = db.get(request.form.get("username"))
+        return render_template("index.html", name=name, count=int(count))
 
-    # render plain text nicely in HTML
-    html_text = font.renderText(message)\
-            .replace(" ","&nbsp;")\
-            .replace(">","&gt;")\
-            .replace("<","&lt;")\
-            .replace("\n","<br>")
-
-    # use a monospace font so everything lines up as expected
-    return "<html><body style='font-family: mono;'>" + html_text + "</body></html>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+@application.route("/prova")
+def prova():
+        return "Prova"
+   
