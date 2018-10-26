a91933421e4900eb583d357815040afb42b70fe2
belerico <belerico@pop-os.localdomain>
2018-10-26 13:35:30 +0200

    Added API to work with both Nginx and uWSGI web server uWSGI API now talk both http and uwsgi on different ports

--- api/api.py
+++ api/api.py
@@ -1,55 +1,77 @@
-from flask import Flask, request, render_template, jsonify, json
+from flask import Flask, request, jsonify, abort
+from flask_restful import Resource, Api
 from redis import StrictRedis
-from operator import itemgetter
 import logging
 
-webapp = Flask(__name__)
+flaskAPI = Flask(__name__)
+api = Api(flaskAPI)
 db = StrictRedis(host='redis', port=6379, db=0)
 
-@webapp.before_first_request
-def setup_logging():
+def get_all_user():
+        data = []
+        for key in db.scan_iter():
+                data.append({'count':db.get(key).decode('utf-8'),'username':key.decode('utf-8')})
+        return data
+
+def sort_json(data, type, key_func):
+        if (type == "desc"):
+                return sorted(data, key=key_func, reverse=True)
+        else:
+                return sorted(data, key=key_func)
+
+@flaskAPI.before_first_request
+def __setup_logging():
     logging.getLogger().setLevel(logging.INFO)
 
-@webapp.route("/greetings", methods=["POST"])
-def greetings():
-        db.incr(request.form.get("username"))
-        name = request.form.get("username")
-        count = db.get(request.form.get("username"))
-        return render_template("index.html", deleted=False, name=name, count=count.decode('utf-8'))
+class UserActions(Resource):
+        def get(self, username):
+                count = db.get(username)
+                if count is None:
+                        abort(404)
+                else:
+                        return jsonify({'count':count.decode('utf-8'),'username':username})
+        
+        def delete(self, username):
+                deleted = db.delete(username)
+                if deleted == 0:
+                        abort(404)
+                else:
+                        return '', 200  
+        
+        def post(self, username):
+                return jsonify(db.incr(username))
 
-@webapp.route("/api/all")
-def get_all():
-        data = []
-        for key in db.scan_iter():
-                data.append({'username': key.decode('utf-8'), 'count': db.get(key).decode('utf-8')})
-        return jsonify(data)
+class AllUsersActions(Resource):
+        def get(self):
+                data = get_all_user()
+                return jsonify(data)
+      
+        def delete(self):
+                if db.dbsize() == 0:
+                        abort(404)
+                else: 
+                        for key in db.scan_iter():
+                                db.delete(key)
+                        return '', 200
 
-@webapp.route("/api/delete/<username>")
-def delete_user(username):
-        db.delete(username)
-        return render_template("index.html", deleted=True, name=username)
+class GetAllUsersSorted(Resource):
+        def get(self, type):
+                data = sort_json(get_all_user(), type, lambda x: (int(x['count']), x['username']))
+                return jsonify(data)
 
+class Func(Resource):
+        def get(self, type):
+                data = get_all_user()
+                if (type == "max"):
+                        max_tuple = max(data, key=lambda x: int(x['count']))
+                        max_tuples = [{"count":i["count"],"username":i["username"]} for i in data if i["count"]==max_tuple["count"]]
+                        return jsonify(max_tuples)
+                elif (type == "min"):
+                        min_tuple = min(data, key=lambda x: int(x['count']))
+                        min_tuples = [{"count":i["count"],"username":i["username"]} for i in data if i["count"]==min_tuple["count"]]
+                        return jsonify(min_tuples)
 
-def sort_by_count(d):
-    '''a helper function for sorting'''
-    return d['count']
-
-@webapp.route("/api/max")
-def get_max():
-        data = []
-        for key in db.scan_iter():
-                data.append({'username': key.decode('utf-8'), 'count': db.get(key).decode('utf-8')})
-        logging.info(data)
-        return sorted(data, key=sort_by_count)
-
-@webapp.route("/api/<username>")
-def get_count(username):
-        if db.get(username) is None:
-                return jsonify(username="Key not found"), 404
-        else:
-                return jsonify(username=username, count=db.get(username).decode('utf-8')), 200
-
-@webapp.route("/prova")
-def prova():
-        return "Prova"
-   
+api.add_resource(UserActions, '/api/<username>')
+api.add_resource(AllUsersActions, '/api/all')
+api.add_resource(Func, '/api/func/<type>')
+api.add_resource(GetAllUsersSorted, '/api/func/sort/<type>')
--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,23 +1,45 @@
-from flask import Flask, request, render_template, jsonify, json
+from flask import Flask, request, render_template, Markup
 from redis import StrictRedis
-from operator import itemgetter
+import requests
 import logging
 
 webapp = Flask(__name__)
-db = StrictRedis(host='redis', port=6379, db=0)
 
 @webapp.before_first_request
 def setup_logging():
     logging.getLogger().setLevel(logging.INFO)
 
-@webapp.route("/greetings", methods=["POST"])
+@webapp.route("/greetings", methods=["GET"])
 def greetings():
-        db.incr(request.form.get("username"))
-        name = request.form.get("username")
-        count = db.get(request.form.get("username"))
-        return render_template("index.html", deleted=False, name=name, count=count.decode('utf-8'))
-        
-@webapp.route("/prova")
-def prova():
-        return "Prova"
+        username = request.args.get("username")
+        requests.post("http://api:7000/api/" + username)
+        r = requests.get("http://api:7000/api/" + username).json()
+        return render_template('index.html', 
+                                text="Hello " + username + ", you\'ve visited this page " + r["count"] + " times", 
+                                delete_link=Markup('<a id="delete" href="http://0.0.0.0/delete?username=' + username + '">Delete me!</a>'))
+
+@webapp.route("/delete", methods=["GET"])
+def delete():
+        username = request.args.get("username")
+        r = requests.delete("http://api:7000/api/" + username)
+        if r.status_code == 200:
+                return render_template('index.html', 
+                                        text="User " + username + " has been deleted", 
+                                        delete_link="")
+        elif r.status_code == 404:
+                return render_template('index.html', 
+                                        text="User " + username + " doesn\'t exist anymore", 
+                                        delete_link="")
+
+@webapp.route("/deleteall", methods=["GET"])
+def deleteall():
+        r = requests.delete("http://api:7000/api/all")
+        if r.status_code == 200:
+                return render_template('index.html', 
+                                        text="All users has been deleted", 
+                                        delete_link="")
+        elif r.status_code == 404:
+                return render_template('index.html', 
+                                        text="No user to delete", 
+                                        delete_link="")
    
