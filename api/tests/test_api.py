5c064bca3de88be8e8320fe9f7397735957fb2a5
Federico Belotti <belo.fede@outlook.com>
2018-10-28 19:35:08 +0000

    Update test_api.py

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -32,9 +32,6 @@ def test_sort_users(client):
     assert r == exp
 
 def test_sort_users_break(client):
-    client.post('/api/user/b')
-    client.post('/api/user/b')
-    client.post('/api/user/a')
     client.post('/api/user/a')
     r = client.get('/api/users/sort/desc').get_json()
     exp = [{"count":"2","username":"a"},{"count":"2","username":"b"}]
